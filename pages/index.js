4026f77e975538925c02c73a80f250836d8f1c5a
Joseph Damiba <josephdamiba@Josephs-MacBook-Pro.local>
2024-08-20 14:32:17 -0700

    initial setup

---
+++ pages/index.js
@@ -0,0 +1,155 @@
+import { useRef } from "react";
+import Header from "../components/Header";
+import ServiceCard from "../components/ServiceCard";
+import Socials from "../components/Socials";
+import WorkCard from "../components/WorkCard";
+import { useIsomorphicLayoutEffect } from "../utils";
+import { stagger } from "../animations";
+import Footer from "../components/Footer";
+import Head from "next/head";
+import Button from "../components/Button";
+import Link from "next/link";
+import Cursor from "../components/Cursor";
+
+// Local Data
+import data from "../data/portfolio.json";
+
+export default function Home() {
+  // Ref
+  const workRef = useRef();
+  const aboutRef = useRef();
+  const textOne = useRef();
+  const textTwo = useRef();
+  const textThree = useRef();
+  const textFour = useRef();
+
+  // Handling Scroll
+  const handleWorkScroll = () => {
+    window.scrollTo({
+      top: workRef.current.offsetTop,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
+  const handleAboutScroll = () => {
+    window.scrollTo({
+      top: aboutRef.current.offsetTop,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useIsomorphicLayoutEffect(() => {
+    stagger(
+      [textOne.current, textTwo.current, textThree.current, textFour.current],
+      { y: 40, x: -10, transform: "scale(0.95) skew(10deg)" },
+      { y: 0, x: 0, transform: "scale(1)" }
+    );
+  }, []);
+
+  return (
+    <div className={`relative ${data.showCursor && "cursor-none"}`}>
+      {data.showCursor && <Cursor />}
+      <Head>
+        <title>{data.name}</title>
+      </Head>
+
+      <div className="gradient-circle"></div>
+      <div className="gradient-circle-bottom"></div>
+
+      <div className="container mx-auto mb-10">
+        <Header
+          handleWorkScroll={handleWorkScroll}
+          handleAboutScroll={handleAboutScroll}
+        />
+        <div className="laptop:mt-20 mt-10">
+          <div className="mt-5">
+            <h1
+              ref={textOne}
+              className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl p-1 tablet:p-2 text-bold w-4/5 mob:w-full laptop:w-4/5"
+            >
+              {data.headerTaglineOne}
+            </h1>
+            <h1
+              ref={textTwo}
+              className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5"
+            >
+              {data.headerTaglineTwo}
+            </h1>
+            <h1
+              ref={textThree}
+              className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5"
+            >
+              {data.headerTaglineThree}
+            </h1>
+            <h1
+              ref={textFour}
+              className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5"
+            >
+              {data.headerTaglineFour}
+            </h1>
+          </div>
+
+          <Socials className="mt-2 laptop:mt-5" />
+        </div>
+        <div className="mt-10 laptop:mt-30 p-2 laptop:p-0" ref={workRef}>
+          <h1 className="text-2xl text-bold">Work.</h1>
+
+          <div className="mt-5 laptop:mt-10 grid grid-cols-1 tablet:grid-cols-2 gap-4">
+            <div
+              className="overflow-hidden rounded-lg p-2 laptop:p-4 first:ml-0 link"
+              onClick={() => window.open("https://www.embedeval.com")}
+            >
+              <div
+                className="relative rounded-lg overflow-hidden transition-all ease-out duration-300 h-48 mob:h-auto"
+                style={{ height: "600px" }}
+              >
+                <img
+                  alt={"A screenshot of the EmbedEval homepage"}
+                  className="h-full w-full object-cover hover:scale-110 transition-all ease-out duration-300"
+                  src={
+                    "https://images.unsplash.com/photo-1487837647815-bbc1f30cd0d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Njl8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60"
+                  }
+                ></img>
+              </div>
+              <h1 className="mt-5 text-3xl font-medium">EmbedEval</h1>
+              <h2 className="text-xl opacity-50">Description</h2>
+            </div>
+            <div
+              className="overflow-hidden rounded-lg p-2 laptop:p-4 first:ml-0 link"
+              onClick={() => window.open("https://www.embedeval.com")}
+            >
+              <div
+                className="relative rounded-lg overflow-hidden transition-all ease-out duration-300 h-48 mob:h-auto"
+                style={{ height: "600px" }}
+              >
+                <iframe
+                  width="100%"
+                  height="100%"
+                  src="https://www.youtube.com/embed/yGZ_L1uLa5g?si=BhXS--t68I4G6XUD"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerpolicy="strict-origin-when-cross-origin"
+                  allowfullscreen
+                ></iframe>
+              </div>
+              <h1 className="mt-5 text-3xl font-medium">
+                What It's Like To Find Out About LangChain
+              </h1>
+              <h2 className="text-xl opacity-50">Description</h2>
+            </div>
+          </div>
+        </div>
+        <div className="mt-10 laptop:mt-40 p-2 laptop:p-0" ref={aboutRef}>
+          <h1 className="tablet:m-10 text-2xl text-bold">About.</h1>
+          <p className="tablet:m-10 mt-2 text-xl laptop:text-3xl w-full laptop:w-3/5">
+            {data.aboutpara}
+          </p>
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
+}
